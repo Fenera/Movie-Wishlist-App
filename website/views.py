907f3cdffb46245b64f8dae40e06517cbfddf0f8
Fenera Taye <fenerataye@gmail.com>
2025-01-13 19:09:58 -0600

    routing stuff

--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+#home page
+@views.route('/', methods= ['POST', 'GET'])
+# home page not accessible if logged out
 def home():
-    return render_template('home.html')
+
+    return render_template('home.html', user=current_user)
+
+# wishlist
+@views.route('/wishlist', methods = ['POST', 'GET'])
+def wishlist():
+    return render_template('wishlist.html')
