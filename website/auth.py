412738d6c69d5eed054c88168ba93dca456d1ebc
Fenera Taye <fenerataye@gmail.com>
2025-01-27 14:54:12 -0600

    fixed sign-up issue

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from flask_login import login_user, current_user, login_required
+from flask_login import login_user, current_user, login_required, logout_user
 
 auth = Blueprint('auth', __name__)
 
@@ -28,7 +28,7 @@ def login():
             else:
                 flash("Password is incorrect, try again!", category="error")
         else:
-            flash("Username does not exist, try again", category="error")
+            flash("Username does not exist, try again or create an account", category="error")
 
 
     return render_template("login.html", user=current_user)
@@ -40,37 +40,29 @@ def sign_up():
     if request.method == 'POST': # register
 
         # get information from entry
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password1 = request.form.get('password') # first password entered
-        password2 = request.form.get('password2') # confirmation password
+        email = request.form.get('email').strip()
+        username = request.form.get('username').strip()
+        password1 = request.form.get('password').strip() # first password entered
+        password2 = request.form.get('password2').strip() # confirmation password
         
-        user_by_email = User.query.filter_by(email=email).first() # fetches the first user object that matches the provided email
-        user_by_username = User.query.filter_by(username=username).first() # same as above but for username
-    
-        # remember information so input boxes don't clear when refreshing or flashing errors
         if not email or not username or not password1 or not password2: # check for empty fields
-            flash("Please fill out all boxes", category="danger")
-            # store the current values in the session
-            session['email'] = email
-            session['username'] = username
-            session['password1'] = password1
-            session['password2'] = password2
-            # redirect to the same page(might be unnecessary)
-            return redirect(url_for('auth.sign_up'))      
+            flash("Please fill out all the fields", category="error")  
+        else:
+            user_by_email = User.query.filter_by(email=email).first() # fetches the first user object that matches the provided email
+            user_by_username = User.query.filter_by(username=username).first() # same as above but for username
 
         if user_by_email: # a user exists in the db with the same email
-            flash("Email is already in use", category="danger")
-        elif user_by_username:
-            flash("Username already in use", category="danger")
+            flash("Email is already in use", category="error")
+        elif user_by_username: # a user exists in the db with the same username
+            flash("Username already in use", category="error") 
         elif len(username) < 4: # length at least 4
-            flash("Username must be at least 4 characters")
+            flash("Username must be at least 4 characters", category="error")
         elif len(email) < 4: # length at least 4
-            flash("Email must be at least 4 characters", category="danger")
+            flash("Email must be at least 4 characters", category="error")
         elif password1 != password2: # original password and confirmation password don't match
-            flash("Password don't match", category="danger")
+            flash("Password don't match", category="error")
         elif len(password1) < 7: # length at least 7
-            flash("Password must be at least 7 characters") 
+            flash("Password must be at least 7 characters", category="error") 
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method="pbkdf2:sha256")) # hash the password(one-way function so password -> hash but not hash -> password) for security purposes(more info: https://www.youtube.com/watch?v=deNIgVyDyJY)   
             
@@ -98,7 +90,11 @@ def sign_up():
     return render_template('sign_up.html', user=current_user, username=username, email=email, password1=password1, password2=password2)
 
 
-# log out 
-# must be logged in(@ logged_in)
-# redirect to login page
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login")) # redirect to login page
+
+
 
