f43607f5376db1f73d2cc9380897b277c71e8bbe
Fenera Taye <fenerataye@gmail.com>
2025-01-31 21:44:06 -0600

    added env variable

--- website/views.py
+++ website/views.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from dotenv import load_dotenv
+import os
+
+
 
 views = Blueprint('views', __name__)
+
+load_dotenv() # load environment variables from .env
+
+api_key = os.getenv("API_KEY") 
+# note to self: pass api key for each dependent page
 
 
 #home page
@@ -9,11 +18,10 @@ views = Blueprint('views', __name__)
 @login_required
 # home page not accessible if logged out
 def home():
+    return render_template('home.html', user=current_user, api_key=api_key) # pass api_key so it is available in .js
 
-    return render_template('home.html', user=current_user)
-
-# wishlist
+# wishlist page
 @views.route('/wishlist', methods = ['POST', 'GET'])
 @login_required
 def wishlist():
-    return render_template('wishlist.html')
+    return render_template('wishlist.html', api_key=api_key)
